30e14d82aa42b2657b26b75c0a29ce81be84ca1c
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-10 15:52:17 +0100

    upddaterar todos status samt hämtar dem med fetch

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
-
-const Todo = ({todo} : {todo: any}) => {
+const Todo = ({todo, onUpdate} : {todo: any, onUpdate: Function}) => {
 
     const statusColor = todo.status === "Ej påbörjad" ? "#E63946" : todo.status === "Påbörjad" ? "#F4D35E" : "#4CAF50"
+    const updateTodo = async (event : any) =>{
+      let newStatus = event.target.value;
 
+      const newTodo = {...Todo, status: newStatus};
+
+      try{
+        const response = await fetch("http://localhost:9000/update/" + todo._id, {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(newTodo)
+        });
+
+        if(!response.ok) {
+            throw Error();
+        }
+
+        onUpdate();
+      } catch (error){
+        throw error;
+      }
+    }
 
   return (
-    <div className="Todo-Card">
+    <div className="Todo-Card" style={{ borderLeft: `5px solid ${statusColor}` }}>
         <h2>{todo.title}</h2>
         <p>{todo.text}</p>
         <p style={{color: statusColor}}>{todo.status}</p>
+
+        <form>
+            <label htmlFor="status">Ändra status:</label>
+            <select name="status" id="status" defaultValue={todo.status} onChange={updateTodo}>
+                <option>Avklarad</option>
+                <option>Påbörjad</option>
+                <option>Ej påbörjad</option>
+            </select>
+        </form>
     </div>
   )
 }
