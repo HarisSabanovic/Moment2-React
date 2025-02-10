a56bdb92f86f943585291afd392c7d37b9af7c86
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-10 12:41:04 +0100

    beroende på status så ändras färgen

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Todo = ({todo} : {todo: any}) => {
+
+    const statusColor = todo.status === "Ej påbörjad" ? "#E63946" : todo.status === "Påbörjad" ? "#F4D35E" : "#4CAF50"
+
+
   return (
     <div className="Todo-Card">
-        
+        <h2>{todo.title}</h2>
+        <p>{todo.text}</p>
+        <p style={{color: statusColor}}>{todo.status}</p>
     </div>
   )
 }
