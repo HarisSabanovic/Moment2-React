92103c49eeb17d91f6996cf5047ba2177c54573f
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-10 11:29:28 +0100

    skapar en inbterface för todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { useState, useEffect } from "react"
 
+interface Todo{
+  title: String,
+  text: String,
+  status: String
+}
+
+
+function App() {
+  // States för komponenten
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Att Göra lista:</h1>
     </>
   )
 }
