817cf2d99477275f490acf0c7158285f801b9ef0
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-10 11:37:11 +0100

    Fetch anrop för att hämta todos från api

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,29 @@ interface Todo{
 
 function App() {
   // States för komponenten
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<Todo | []>([])
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
+  const fetchTodos = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch("http://localhost:9000");
+
+      if (!response.ok) {
+        throw Error("Kunde inte hämta datan " + response.status);
+      }
+
+      const data = await response.json();
+      setTodos(data);
+
+    } catch(error) {
+      setError("Kunde inte hämta data från API");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <>
       <h1>Att Göra lista:</h1>
